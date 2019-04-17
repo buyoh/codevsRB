616e9cb7c365fd00162da0155eedb2ca523368aa
buyoh <japanjetone@live.jp>
2019-04-18 08:08:00 +0900

    #9 gccシェルスクリプト

--- codevsRB/1.sh
+++ codevsRB/1.sh
@@ -1,2 +1,12 @@
-make
+COPT=-O3 -mavx
+
+g++ $COPT -c BattleAI.cpp -o BattleAI.o
+g++ $COPT -c CaseGenerator.cpp -o CaseGenerator.o
+g++ $COPT -c Exec.cpp -o Exec.o
+g++ $COPT -c Game.cpp -o Game.o
+g++ $COPT -c Input.cpp -o Input.o
+g++ $COPT -c Main.cpp -o Main.o
+
+g++ -O3 Main.o Input.o Game.o Exec.o CaseGenerator.o BattleAI.o -lm -o ./codevs
+
 ./codevs
