462fa55d4a1d34573936804ab087512a7cd7443f
舞葉 <japanjetone@live.jp>
2019-04-24 08:18:29 +0900

    gcc 最適化オプションの変更

--- codevsRB/1.sh
+++ codevsRB/1.sh
@@ -1,4 +1,4 @@
-COPT=-O3 -march=native
+COPT='-Ofast -march=native -pthread'
 
 g++ $COPT -c BattleAI.cpp -o BattleAI.o
 g++ $COPT -c CaseGenerator.cpp -o CaseGenerator.o
@@ -7,6 +7,6 @@ g++ $COPT -c Game.cpp -o Game.o
 g++ $COPT -c Input.cpp -o Input.o
 g++ $COPT -c Main.cpp -o Main.o
 
-g++ -O3 Main.o Input.o Game.o Exec.o CaseGenerator.o BattleAI.o -lm -o ./codevs
+g++ -O3 Main.o Input.o Game.o Exec.o CaseGenerator.o BattleAI.o -lm -lpthread -o ./codevs
 
 ./codevs
