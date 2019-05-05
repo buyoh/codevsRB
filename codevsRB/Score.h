19a8e107b29800bda5ce3930f3d373a8f36e0342
舞葉 <japanjetone@live.jp>
2019-05-05 13:09:13 +0900

    😲

--- codevsRB/Score.h
+++ codevsRB/Score.h
@@ -32,20 +32,20 @@ namespace Game {
         constexpr int operator[](int i) const { return data_[i]; }
     };
 
-    class BombScoreT {
-        static constexpr int Limit = 170;
-        int data_[Limit];
-    public:
-        constexpr BombScoreT() :data_() {
-            for (int i = 1; i < Limit; ++i)
-                data_[i] = static_cast<int>(25.0 * cpow(2.0, double(i) / 12.0));
-        }
-        constexpr int operator[](int i) const { return data_[i]; }
-    };
+    // class BombScoreT {
+    //     static constexpr int Limit = 170;
+    //     int data_[Limit];
+    // public:
+    //     constexpr BombScoreT() :data_() {
+    //         for (int i = 1; i < Limit; ++i)
+    //             data_[i] = static_cast<int>(25.0 * cpow<double>(2.0, double(i) / 12.0)); // 有理数だから二分法で簡単に出来なくもない
+    //     }
+    //     constexpr int operator[](int i) const { return data_[i]; }
+    // };
 
     // なんもわからん
     constexpr ChainScoreT ChainScore;
     constexpr ChainSkillScoreT ChainSkillScore;
-    constexpr BombScoreT BombScore;
+    constexpr int BombScore[] = { 0,26,28,29,31,33,35,37,39,42,44,47,50,52,56,59,62,66,70,74,79,84,89,94,100,105,112,118,125,133,141,149,158,168,178,188,200,211,224,237,251,266,282,299,317,336,356,377,400,423,448,475,503,533,565,599,634,672,712,755,800,847,897,951,1007,1067,1131,1198,1269,1345,1425,1510,1600,1695,1795,1902,2015,2135,2262,2397,2539,2690,2850,3020,3200,3390,3591,3805,4031,4271,4525,4794,5079,5381,5701,6040,6400,6780,7183,7610};
 
 }
